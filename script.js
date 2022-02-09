bcd4428b7bf50b97f3811cdd082597b6a6d3dc9f
labstraction <45163263+labstraction@users.noreply.github.com>
2022-02-09 12:39:38 +0100

    line Parser

--- script.js
+++ script.js
@@ -1,111 +1,107 @@
-"use strict";
+// "use strict";
 
-let pippo = "ciao";
+// let pippo = "ciao";
 
-let pluto = 4;
-console.log("🚀 ~ file: script.js ~ line 6 ~ pluto", pluto);
+// let pluto = 4;
+// console.log("🚀 ~ file: script.js ~ line 6 ~ pluto", pluto);
 
-let paperino = false;
-console.log("🚀 ~ file: script.js ~ line 8 ~ paperino", paperino);
+// let paperino = false;
+// console.log("🚀 ~ file: script.js ~ line 8 ~ paperino", paperino);
 
-let array = ["pippo", "pluto", "paperino","pippo", "pluto", "paperino","pippo", "pluto", "paperino","pippo", "pluto", "paperino"];
-console.table(array)
+// let array = ["pippo", "pluto", "paperino","pippo", "pluto", "paperino","pippo", "pluto", "paperino","pippo", "pluto", "paperino"];
+// console.table(array)
 
-let stud1 = {name: "andrea", age:12}
+// let stud1 = {name: "andrea", age:12}
 
-let stud2 = {name: "giovanni", age:13}
+// let stud2 = {name: "giovanni", age:13}
 
-let stud3 = {name: "beatrice", age:16}
+// let stud3 = {name: "beatrice", age:16}
 
-let stud4 = {name: "veronica", age:12}
+// let stud4 = {name: "veronica", age:12}
 
-const array2 = [stud1, stud2, stud3, stud4];
+// const array2 = [stud1, stud2, stud3, stud4];
 
-console.table(array2);
+// console.table(array2);
 
-function checkIfIsEven(NumberToCheck) {
-    return NumberToCheck % 2 === 0;
-}
+// function checkIfIsEven(NumberToCheck) {
+//     return NumberToCheck % 2 === 0;
+// }
 
-function check(n) {
-    return n % 2 === 0;
-}
-
-
-let player = {name: "andrea", surname:"asioli", school: "leopardi", game:"space invader", age:12, address: "via piantelli 12",  score: 112};
-
-console.log("🚀 ~ file: script.js ~ line 37 ~ player", player);
-
-console.log("🚀 ~ file: script.js ~ line 37 ~ player", JSON.stringify(player));
-
-let number = 300;
-
-let isEven = checkIfIsEven(number);
-
-player.score = 200;
-
-class EmptyStringError extends Error{
-    constructor(message){
-        super(message);
-    }
-}
+// function check(n) {
+//     return n % 2 === 0;
+// }
 
 
-class InvalidStringError extends Error{
-    constructor(message){
-        super(message);
-    }
-}
+// let player = {name: "andrea", surname:"asioli", school: "leopardi", game:"space invader", age:12, address: "via piantelli 12",  score: 112};
 
-class InvalidStringError extends Error{
-    constructor(message, partialResult){
-        super(message);
-        this.partialResult = partialResult
-    }
-}
+// console.log("🚀 ~ file: script.js ~ line 37 ~ player", player);
 
-function parseStringToNumber(string) {
-    if (string.length === 0) {
-        throw new EmptyStringError("ciao")
-    }
-    let stringNumber = string;
-    if (string.includes(",")) {
-        stringNumber = string.replace(",", ".")
-    }
-    const number = parseFloat(stringNumber)
-    if (isNaN(number)){
-        throw new InvalidStringError("stringa non valida")
-    }
-    return number;
-}
+// console.log("🚀 ~ file: script.js ~ line 37 ~ player", JSON.stringify(player));
 
-let age;
+// let number = 300;
 
-try {
+// let isEven = checkIfIsEven(number);
 
-    age = parseStringToNumber("");
-    console.log("age: ",age);
+// player.score = 200;
 
-} catch (error) {
+// class EmptyStringError extends Error{
+//     constructor(message){
+//         super(message);
+//     }
+// }
 
-    if (error instanceof EmptyStringError) {
-        console.log(error.message)
-        age = 0;
-        console.log("age: ",age);
-    } else{
-        console.log(error.message)
-        console.log("inserisci di nuovo")
-    }
 
-    // if (error.message === "stringa vuota") {
-    //     age = 0;
-    //     console.log("age: ",age);
-    // } else {
-    //     console.log(error.message)
-    //     console.log("inserisci di nuovo")
-    // }
+// class InvalidStringError extends Error{
+//     constructor(message){
+//         super(message);
+//     }
+// }
+
+
+
+
+// function parseStringToNumber(string) {
+//     if (string.length === 0) {
+//         throw new EmptyStringError("ciao")
+//     }
+//     let stringNumber = string;
+//     if (string.includes(",")) {
+//         stringNumber = string.replace(",", ".")
+//     }
+//     const number = parseFloat(stringNumber)
+//     if (isNaN(number)){
+//         throw new InvalidStringError("stringa non valida")
+//     }
+//     return number;
+// }
+
+// let age;
+
+// try {
+
+//     age = parseStringToNumber("");
+//     console.log("age: ",age);
+
+// } catch (error) {
+
+//     if (error instanceof EmptyStringError) {
+//         console.log(error.message)
+//         age = 0;
+//         console.log("age: ",age);
+//     } else{
+//         console.log(error.message)
+//         console.log("inserisci di nuovo")
+//     }
+
+//     // if (error.message === "stringa vuota") {
+//     //     age = 0;
+//     //     console.log("age: ",age);
+//     // } else {
+//     //     console.log(error.message)
+//     //     console.log("inserisci di nuovo")
+//     // }
     
-}
+// }
 
 
 
@@ -118,3 +114,20 @@ try {
 // console.log(parseStringToNumber("2pippo"));
 // console.log(parseStringToNumber("pippo2"));
 // console.log(parseStringToNumber("pippo"));
+
+
+let string = "12; pippo; 1.2; false; ecco"
+
+
+let numbersArray = [];
+
+try {
+    numbersArray = Parser.parseCSVLine(string);
+} catch (error) {
+    console.log(error.message)
+    if (error instanceof PartialInvalidStringError) {
+        numbersArray = error.partialResult;
+    }
+}
+
+console.log(numbersArray);
